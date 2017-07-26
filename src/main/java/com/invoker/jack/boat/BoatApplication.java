254fca150d23013f955f9dd44eb744d6e84f8606
Invoker_liu <liu54875487@163.com>
2017-07-27 01:25:03 +0800

    Initial commit

---
+++ src/main/java/com/invoker/jack/boat/BoatApplication.java
@@ -0,0 +1,12 @@
+package com.invoker.jack.boat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatApplication.class, args);
+	}
+}
